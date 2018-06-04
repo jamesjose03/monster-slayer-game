d0454a6c14f3444329d01e923ca1959e36a4a033
James Kaviyil Jose <jamesjose03@gmail.com>
2018-06-04 17:56:04 +0530

    Action Log Completed

--- js/script.js
+++ js/script.js
@@ -3,18 +3,24 @@ new Vue ({
     data: {
         playerHealth: 100,
         monsterHealth: 100,
-        gameState: false
+        gameState: false,
+        turns: []
     },
     methods: {
         start: function(){
             this.gameState= true;
             this.monsterHealth= 100;
             this.playerHealth = 100;
+            this.turns = [];
 
         },
         attack: function(){
             var damage = this.damageDealt(3,10)
             this.monsterHealth -= damage;
+            this.turns.unshift({
+                isPlayer: true,
+                text: 'Player hits Monster for '+ damage
+            })
 
             if(this.checkWin()){
                 return;
@@ -25,6 +31,10 @@ new Vue ({
         special_attack: function(){
             var damage = this.damageDealt(10,20)
             this.monsterHealth -= damage;
+            this.turns.unshift({
+                isPlayer: true,
+                text: 'Player hits Monster hard for '+ damage
+            })
 
             if(this.checkWin()){
                 return;
@@ -39,15 +49,24 @@ new Vue ({
             }else{
                 this.playerHealth = 100;
             }
+            this.turns.unshift({
+                isPlayer: true,
+                text: 'Player heals for 10 '
+            })
             this.monsterAttacks();
         },
         give_up: function(){
-
+            this.gameState = false;
         },
         monsterAttacks: function(){
             damage = this.damageDealt(5,12);
             this.playerHealth -= damage;
             this.checkWin();
+            this.turns.unshift({
+                isPlayer: false,
+                text: 'Monster hits Player for '+ damage
+            })
+
         },
         damageDealt: function(min,max){
             return Math.max(Math.floor(Math.random() * max) + 1 , min);
