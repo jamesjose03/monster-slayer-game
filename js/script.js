653523a48cdd505e1f357a8b3a997dee92fec0cf
James Kaviyil Jose <jamesjose03@gmail.com>
2018-06-04 17:35:30 +0530

    Implemented the Heal option

--- js/script.js
+++ js/script.js
@@ -34,7 +34,12 @@ new Vue ({
             
         },
         heal: function(){
-
+            if(this.playerHealth<=90){
+                this.playerHealth += 10;
+            }else{
+                this.playerHealth = 100;
+            }
+            this.monsterAttacks();
         },
         give_up: function(){
 
