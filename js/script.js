606ca22e858012b3712d5abcd40396099c386fac
James Kaviyil Jose <jamesjose03@gmail.com>
2018-06-04 15:19:09 +0530

    Implemented Attack Method

--- js/script.js
+++ js/script.js
@@ -11,6 +11,39 @@ new Vue ({
             this.monsterHealth= 100;
             this.playerHealth = 100;
 
+        },
+        attack: function(){
+            var max = 10;
+            var min = 3;
+            var damage = Math.max(Math.floor(Math.random() * max) + 1 , min);
+            this.monsterHealth -= damage;
+
+            if(this.monsterHealth <=0)
+            {
+                alert('You won!')
+                this.gameState = false;
+                return;
+            }
+            max = 12;
+            min = 5;
+            damage = Math.max(Math.floor(Math.random() * max) + 1 , min);
+            this.playerHealth -= damage;
+
+            if(this.playerHealth <=0)
+            {
+                alert('You lost!')
+                this.gameState = false;
+                
+            }
+        },
+        special_attack: function(){
+
+        },
+        heal: function(){
+
+        },
+        give_up: function(){
+
         }
     }
 })
